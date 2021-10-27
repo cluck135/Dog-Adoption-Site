ce2af507564b1110de23d813ab569cb08bcafdc8
npduval <nickman92186@yahoo.com>
2021-10-26 20:34:48 -0400

    completed adoption form

--- adopt-form.js
+++ adopt-form.js
@@ -1,1 +1,18 @@
-//SAVE FORM TO LOCAL STORAGE
+// upon submit show message
+
+const Page1 = document.querySelector(".formPage");
+const Page2 = document.querySelector(".submitPage");
+const submitBtn = document.querySelector("#submit");
+const homeBtn = document.querySelector("#home");
+
+submitBtn.addEventListener("click", function(){
+    
+    Page1.style.display = 'none';
+    Page2.style.display = 'block';
+  
+  });
+
+  homeBtn.addEventListener("click", function(){
+    window.location.assign("https://cluck135.github.io/Dog-Adoption-Site/")
+
+  });
