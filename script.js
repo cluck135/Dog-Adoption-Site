ed573d184108696be3a409ada9dd8c43326af117
Scott LaBelle <scott.labelle@snhu.edu>
2021-10-27 21:20:54 -0400

    Added the ability to display the breeds underneath the random breeds. Added a box shadow effect to the random dog images

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let randomBreedList = "https://dog.ceo/api/breeds/list/all";
 let randomDoggo = "https://dog.ceo/api/breeds/image/random/3";
 
 let message = "Breed of that type is not available for adoption";
-let placeholder = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcountrylabradoodles.com%2Fwp-content%2Fuploads%2F2017%2F11%2Fdog-coming-soon1-400x267.jpeg&f=1&nofb=1";
+let placeholder =
+  "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcountrylabradoodles.com%2Fwp-content%2Fuploads%2F2017%2F11%2Fdog-coming-soon1-400x267.jpeg&f=1&nofb=1";
 
 let breedOne = document.getElementById("random1");
 let breedTwo = document.getElementById("random2");
@@ -12,6 +13,9 @@ let randImg = document.querySelectorAll(".randImg");
 let adoptionList = document.getElementById("adoptionList");
 let randomBtn = document.getElementById("random-button");
 let errorMsg = document.getElementById("error-message");
+let randDogBreedOne = document.getElementById("rand-dog-breed-one");
+let randDogBreedTwo = document.getElementById("rand-dog-breed-two");
+let randDogBreedThree = document.getElementById("rand-dog-breed-three");
 
 displayRandomDogs(randomDoggo);
 
@@ -41,6 +45,7 @@ function displayRandomDogs(randomDoggo) {
             .split("/")[0]
             .split("-")[0];
           breeds.push(firstPic);
+          randDogBreedOne.append(breeds[0]);
           localStorage.setItem("breeds", JSON.stringify(breeds));
         } else if (i == 1) {
           breedTwo.src = img;
@@ -49,6 +54,7 @@ function displayRandomDogs(randomDoggo) {
             .split("/")[0]
             .split("-")[0];
           breeds.push(secondPic);
+          randDogBreedTwo.append(breeds[1]);
           localStorage.setItem("breeds", JSON.stringify(breeds));
         } else if (i === 2) {
           breedThree.src = img;
@@ -57,20 +63,20 @@ function displayRandomDogs(randomDoggo) {
             .split("/")[0]
             .split("-")[0];
           breeds.push(thirdPic);
+          randDogBreedThree.append(breeds[2]);
           localStorage.setItem("breeds", JSON.stringify(breeds));
         }
       }
     });
 }
 
-
 fetch("https://api.petfinder.com/v2/oauth2/token", {
   body: "grant_type=client_credentials&client_id=5gyYpH99UKTQsKQ27CtKPorIMTjWhCWZf1Ky2kcDCHF9OiISmv&client_secret=Otrvy30rjrgjAu2XkRF9lyh3xrmrg3enhCOucfSH",
   headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
+    "Content-Type": "application/x-www-form-urlencoded",
   },
-  method: "POST"
-  })
+  method: "POST",
+})
   .then(function (response) {
     //console.log(response);
     if (response.ok) {
@@ -84,20 +90,14 @@ fetch("https://api.petfinder.com/v2/oauth2/token", {
     localStorage.setItem("auth", JSON.stringify(data));
   });
 
-
 let randAuthToken = JSON.parse(localStorage.getItem("auth"));
-let authToken = randAuthToken['access_token'];
+let authToken = randAuthToken["access_token"];
 console.log(authToken);
-
-
 
 const myHeaders = new Headers();
 
 myHeaders.append("Content-Type", "application/json");
-myHeaders.append(
-  "Authorization",
-  "Bearer "+ authToken +""
-);
+myHeaders.append("Authorization", "Bearer " + authToken + "");
 
 // fetch("https://api.petfinder.com/v2/types/Dog/breeds", {
 //   method: "GET",
@@ -125,7 +125,6 @@ fetch("https://api.petfinder.com/v2/animals?type=dog&page=1", {
     if (response.ok) {
       return response.json();
     } else {
-        
       throw new Error(message || response.status);
     }
   })
@@ -133,7 +132,6 @@ fetch("https://api.petfinder.com/v2/animals?type=dog&page=1", {
     console.log(data);
     let Doggo2 = data;
     for (let i = 0; i < Doggo2.animals.length; i++) {
-        
       let picUrl = "";
       let dogDesc = Doggo2.animals[i].description;
       let dogName = Doggo2.animals[i].name;
@@ -165,11 +163,10 @@ fetch("https://api.petfinder.com/v2/animals?type=dog&page=1", {
       }
       pic.setAttribute("class", "dogImg");
 
-      
       anchor.setAttribute("href", url);
       anchor.appendChild(pic);
       formAnchor.appendChild(formBtn);
-      
+
       div.appendChild(name);
       div.appendChild(desc);
       div.appendChild(formAnchor);
@@ -179,7 +176,6 @@ fetch("https://api.petfinder.com/v2/animals?type=dog&page=1", {
 
       adoptionList.appendChild(li);
     }
-    
   });
 
 function removeAllChildNodes(parent) {
@@ -203,10 +199,11 @@ randDoggos.addEventListener("click", function (e) {
     .then(function (response) {
       console.log(response);
       if (response.ok) {
-          errorMsg.innerText =""
+        errorMsg.innerText = "";
         return response.json();
       } else {
-            errorMsg.innerText = "That dog breed is not up for adoption! Try another one!";
+        errorMsg.innerText =
+          "That dog breed is not up for adoption! Try another one!";
         throw new Error(message || response.status);
       }
     })
@@ -250,7 +247,7 @@ randDoggos.addEventListener("click", function (e) {
         anchor.setAttribute("href", url);
         anchor.appendChild(pic);
         formAnchor.appendChild(formBtn);
-        
+
         div.appendChild(name);
         div.appendChild(desc);
         div.appendChild(formAnchor);
@@ -264,5 +261,8 @@ randDoggos.addEventListener("click", function (e) {
 });
 
 randomBtn.addEventListener("click", function () {
+  randDogBreedOne.innerHTML = "";
+  randDogBreedTwo.innerHTML = "";
+  randDogBreedThree.innerHTML = "";
   displayRandomDogs(randomDoggo);
 });
