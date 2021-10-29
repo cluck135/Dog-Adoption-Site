a4290f6c0d28c2f3a419737de7971f2776505b05
npduval <nickman92186@yahoo.com>
2021-10-28 21:31:55 -0400

    added validation to adotion form and completed styling

--- adopt-form.js
+++ adopt-form.js
@@ -1,16 +1,30 @@
-// upon submit show message
 
 const Page1 = document.querySelector(".formPage");
 const Page2 = document.querySelector(".submitPage");
 const submitBtn = document.querySelector("#submit");
 const homeBtn = document.querySelector("#home");
+const nameEl = document.querySelector("#nameValue");
+const addressEl = document.querySelector("#addressValue");
+const emailEl = document.querySelector("#emailValue");
+const cellEl = document.querySelector("#cellValue");
+const errorEl = document.querySelector("#error");
+
 
 submitBtn.addEventListener("click", function(){
-    
-    Page1.style.display = 'none';
-    Page2.style.display = 'block';
-  
-  });
+
+    if (!nameEl.value) {
+        errorEl.innerText = "Please enter valid contact information!";
+    } else if (!addressEl.value) {
+        errorEl.innerText = "Please enter valid contact information!";
+    } else if (!emailValue) {
+      errorEl.innerText = "Please enter valid contact information!";  
+    } else if (!cellValue){
+      errorEl.innerText = "Please enter valid contact information!";
+    } else {
+      Page1.style.display = 'none';
+      Page2.style.display = 'block';
+    }
+});
 
   homeBtn.addEventListener("click", function(){
     window.location.assign("https://cluck135.github.io/Dog-Adoption-Site/")
